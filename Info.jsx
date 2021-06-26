4c54da2c5d197b1ac00c9881765fecbba225c242
Leo <47278525+leotryout@users.noreply.github.com>
2021-06-26 22:15:01 +0100

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript Framework</h1>
+      <p> Pure Basic JS and React.js</p>
+    </div>
+  );
+}
+
+export default Info;
